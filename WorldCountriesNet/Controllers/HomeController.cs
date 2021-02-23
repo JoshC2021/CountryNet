f32cdf0de26327456ee44c4ae82381df8a9c3135
Josh <josh_2021@yahoo.com>
2021-02-23 09:04:51 -0500

    initial commit

---
+++ WorldCountriesNet/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCountriesNet.Models;
+
+namespace WorldCountriesNet.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        IList<Country> countriesList = new List<Country>
+        {
+            new Country{Name = "USA", OfficialLanguage = "English", HelloWorld = "Hello", Description = "Best fries", PrimaryColors = new[] {"Red","White","Blue" } },
+
+            new Country { Name = "Germany", OfficialLanguage = "German", HelloWorld = "Guten Tag", Description = "Best composers", PrimaryColors = new[] { "Black", "Red", "Gold" } },
+
+            new Country { Name = "Italy", OfficialLanguage = "Italian", HelloWorld = "Ciao", Description = "Best pizza", PrimaryColors = new[] { "Green", "White", "Red" } },
+        };
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["countries"] = countriesList;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string country)
+        {
+            TempData["country"] = country;
+            return RedirectToAction("Details");
+        }
+
+        public IActionResult Description()
+        {
+            ViewData["countries"] = countriesList;
+            return View();
+        }
+        public IActionResult Details()
+        {
+            ViewData["countries"] = countriesList;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WorldCountriesNet/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCountriesNet.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string OfficialLanguage { get; set; }
+        public string HelloWorld { get; set; }
+        public string Description { get; set; }
+        public string[] PrimaryColors { get; set; }
+    }
+}
